52dfb09ce2a1226ad7141da3a97b4878dbd1af25
Zaiyad7 <ZaiyadZakaria@live.co.uk>
2023-08-23 23:01:15 +0100

    tested sellCar method in dealership test file

---
+++ Customer.test.js
@@ -0,0 +1,12 @@
+const Customer = require('./Customer');
+const Car = require('./Car');
+
+test('Customer can buy a car', () => {
+  const customer = new Customer('Bob', 30000);
+  const car = new Car('Toyota', 25000, 'Petrol');
+
+  customer.buyCar(car);
+
+  expect(customer.car).toBe(car);
+  expect(customer.wallet).toBe(5000); 
+});
--- Dealership.test.js
+++ Dealership.test.js
@@ -1,7 +1,8 @@
 const Dealership = require('./Dealership');
 const Car = require('./Car');
+const Customer = require('./Customer');
 
-test('Dealership methods work correctly', () => {
+test('Dealership methods work', () => {
   const dealership = new Dealership('AutoTrader', 5);
 
   const car1 = new Car('Toyota', 25000, 'Petrol');
@@ -15,4 +16,22 @@ test('Dealership methods work correctly', () => {
   expect(dealership.getCarsByManufacturer('Toyota')).toEqual([car1]);
   expect(dealership.calculateTotalValue()).toBe(53000);
 });
+
+test('Dealership can sell car to customer', () => {
+    const dealership = new Dealership('AutoTrader', 5);
+    const customer = new Customer('Ezio', 40000);
+  
+    const car1 = new Car('Toyota', 25000, 'Petrol');
+    const car2 = new Car('Honda', 28000, 'Electric');
+  
+    dealership.addCarToStock(car1);
+    dealership.addCarToStock(car2);
+  
+    const soldCar = dealership.sellCar('Toyota', customer);
+  
+    expect(soldCar).toEqual(car1);
+    expect(dealership.countCarsInStock()).toBe(1);
+    expect(customer.car).toBe(car1);
+    expect(customer.wallet).toBe(15000); 
+  });
     
